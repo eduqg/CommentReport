9e212ece89051d65b31d38655311e499f8dc663c
eduqg <eduardoqgomes@gmail.com>
2018-09-19 23:32:08 -0300

    Creating Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :reports
+  resources :comments
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
